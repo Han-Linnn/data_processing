b93047607bf59b473002fdf6244dd9a4d9711a1d
Han-Linnn <95120806+Han-Linnn@users.noreply.github.com>
2022-06-03 04:10:32 +0200

    Update test_preprocessing.py

--- test_preprocessing.py
+++ test_preprocessing.py
@@ -4,7 +4,7 @@ import unittest
 class TestStringMethods(unittest.TestCase):
     # local testing
     def test_preprocessing(self):
-        self.assertEqual( preProcessing('/data', '/data'), "pre_processing")
+        self.assertEqual(preProcessing('/data', '/data'), "pre_processing")
 
 if __name__=="__main__":
     unittest.main()
