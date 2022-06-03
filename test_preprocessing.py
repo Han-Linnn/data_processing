76d651b1cffbcc59f9a2344c195a194d2a66b62b
Han-Linnn <95120806+Han-Linnn@users.noreply.github.com>
2022-06-03 03:06:04 +0200

    Update test_preprocessing.py

--- test_preprocessing.py
+++ test_preprocessing.py
@@ -5,7 +5,7 @@ class TestStringMethods(unittest.TestCase):
     # local testing
     def test_preprocessing(self):
         self.assertEqual( preProcessing('/data', '/data'), "pre_processing")
-
+        print( preProcessing('/data', '/data') )
     # def test_add():
     #     assert 1+1 == 2 '1+1 == 2 is right'
 
